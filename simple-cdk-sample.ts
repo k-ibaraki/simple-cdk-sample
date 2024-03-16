47a64a8d268b40e435389846e8f9f1875a5ca2c3
k-ibaraki <ibaraki@ncdc.co.jp>
2024-03-16 18:32:33 +0900

    stack名の修正

--- simple-cdk-sample.ts
+++ simple-cdk-sample.ts
@@ -1,4 +1,4 @@
 import * as cdk from 'aws-cdk-lib';
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, "simple-cdk-sample-stack", {});
+const stack = new cdk.Stack(app, "SimpleCdkSampleStack", {});
