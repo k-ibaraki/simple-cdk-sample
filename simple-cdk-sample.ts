2606d18c9164d06c4ba86c0c3339616da22a4d9a
k-ibaraki <ibaraki@ncdc.co.jp>
2024-03-16 20:28:08 +0900

    コメントの追加

--- simple-cdk-sample.ts
+++ simple-cdk-sample.ts
@@ -2,10 +2,12 @@ import * as cdk from 'aws-cdk-lib';
 import { ApiGatewayToLambda } from '@aws-solutions-constructs/aws-apigateway-lambda';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
+// CDKのAppを定義
 const app = new cdk.App();
+// Appの配下にStackを定義
 const stack = new cdk.Stack(app, "SimpleCdkSampleStack", {});
-
-const apiLambda = new ApiGatewayToLambda(stack, 'ApiGatewayToLambda', {
+// Stackの配下にConstruct(ApiGatewayToLambda)を定義
+const apiToLambda = new ApiGatewayToLambda(stack, 'ApiGatewayToLambda', {
   lambdaFunctionProps: {
     runtime: lambda.Runtime.NODEJS_20_X,
     handler: 'index.handler',
