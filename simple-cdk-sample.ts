48a8247993adc6b98be0023b24d10a4a0fd309cb
k-ibaraki <ibaraki@ncdc.co.jp>
2024-03-16 19:36:06 +0900

    リソース名を修正

--- simple-cdk-sample.ts
+++ simple-cdk-sample.ts
@@ -5,13 +5,14 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 const app = new cdk.App();
 const stack = new cdk.Stack(app, "SimpleCdkSampleStack", {});
 
-const apiLambda = new ApiGatewayToLambda(stack, 'ApiGatewayToLambdaPattern', {
+const apiLambda = new ApiGatewayToLambda(stack, 'ApiGatewayToLambda', {
   lambdaFunctionProps: {
     runtime: lambda.Runtime.NODEJS_20_X,
     handler: 'index.handler',
     code: lambda.Code.fromAsset(`lambda`)
   },
   apiGatewayProps: {
+    restApiName: 'SimpleCdkSampleApi',
     defaultMethodOptions: {
       authorizationType: 'NONE',
     },
